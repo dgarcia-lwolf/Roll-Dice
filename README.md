59cc918387c8dfe6e41e06ef023955781788aeb7
Danilo Garcia <dgarcia@lwolf.com>
2023-12-27 09:50:01 -0500

    Section 2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Roll Dice Exercise
+
+Flutter project.
+
+- Course: [A Complete Guide to the Flutter SDK &amp; Flutter Framework for building native iOS and Android apps](https://lwolf.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/)
+- Instructor: [Maximilian Schwarzmüller](https://lwolf.udemy.com/user/maximilian-schwarzmuller/)
---
+++ lib/dice_roller.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+
+  void RollDice() {
+    int diceRoll = 1 + randomizer.nextInt(6);
+    setState(() {
+      currentDiceRoll = diceRoll;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+            onPressed: RollDice,
+            child: const Text('Roll Dice'),
+            style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20,
+                // ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28)))
+      ],
+    );
+  }
+}
